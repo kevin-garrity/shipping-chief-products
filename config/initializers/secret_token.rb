d4012fa8511794c1d5f0e74fa576e45e63317fc8
Michael Johnston <lastobelus@mac.com>
2012-12-27 17:20:20 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorldShippingCalculator::Application.config.secret_token = '88e543f4c629c988a969c9cf624680a5dc353affe15dce4e86c78411caec469527cb2ec77813d6911af2781eb87b0ce7cb4c52ba9033e44f862c29648fa02629'
