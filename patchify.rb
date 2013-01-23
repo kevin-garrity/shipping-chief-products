57987cc36814e77cb7ac155433602e55a9e54eaa
Zeigfreid <various.authors@gmail.com>
2013-01-22 17:56:16 -0800

    Patched patchify :)

--- patchify.rb
+++ patchify.rb
@@ -2,8 +2,8 @@
 # for each file in the patchify dir we will upload the file
 
 puts "applying patch"
-ENV['PATCHIFY_DIR'] = "patchify"
+ENV['PATCHIFY_DIR'] = "shopify_theme_modifications"
 
-Dir.glob(File.join("patchify", "*/*")).reverse.each do |file|
-  puts `shoifydev upload #{file}`
+Dir.glob(File.join(ENV['PATCHIFY_DIR'], "*/*")).reverse.each do |file|
+  puts `shopifydev upload #{file[file.index("/") + 1, file.length - 1]}`
 end
