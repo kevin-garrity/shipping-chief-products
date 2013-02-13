dfeb09b5a25590072f1dba59537044533d9d5ccf
Richard Zhou <richard.zhou@sage.com>
2013-02-13 15:11:03 -0800

    fix url for staging

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -8,7 +8,7 @@ AppConfig = ConfigSpartan.create do
   end
 end
 
-DOMAIN_NAMES = {"staging" => "test.biguglyurl.com", "development" => "localhost:3000", "production" =>  "shipping.webifytechnology.com", "test" => "localhost:3000"}
+DOMAIN_NAMES = {"staging" => "shipping-staging.herokuapp.com", "development" => "localhost:3000", "production" =>  "shipping.webifytechnology.com", "test" => "localhost:3000"}
 
 Rails.logger = Logger.new(STDOUT)
 Rails.logger = Log4r::Logger.new("Application Log")
