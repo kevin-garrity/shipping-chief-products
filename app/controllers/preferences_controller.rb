95d919c9e2a009057b28e2f6871cd81447d0c13a
Richard Zhou <richard.zhou@sage.com>
2013-02-15 23:57:52 -0800

    store shipping product variant in metafields and retrieve variant id  in liquid

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,6 +3,8 @@ class PreferencesController < ApplicationController
   before_filter :check_payment
   
   def show
+    check_shipping_product_exists
+    
     @preference = Preference.find_by_shop_url(session[:shopify].shop.domain)
     @preference = Preference.new if @preference.nil?
     
@@ -14,6 +16,7 @@ class PreferencesController < ApplicationController
 
   # GET /preference/edit
   def edit
+    check_shipping_product_exists
     @preference = Preference.find_by_shop_url(session[:shopify].shop.domain)
     @preference = Preference.new if @preference.nil?
     
@@ -46,6 +49,47 @@ class PreferencesController < ApplicationController
   end
 
 private 
+  def check_shipping_product_exists
+    fields = "id,title, handle"
+    search_params = {:fields=>fields, :limit => 1, :page=>1}      
+    search_params = search_params.merge({:handle=>"webify-shipping-app-product"})
+  
+    @products = ShopifyAPI::Product.find(:all, :params => search_params)
+    
+    if @products.length == 0
+      #try to create
+      prod = ShopifyAPI::Product.new(
+      :title =>"Shipping",
+      :handle=>"webify-shipping-app-product",
+      :product_type=>"product",
+      :variants => ShopifyAPI::Variant.new(:price => 0.01)
+      )
+      prod.save!      
+    else
+      prod = @products[0]
+    end
+    
+    @vars = ShopifyAPI::Variant.find(:all, :params=>{:product_id => prod.id})
+    
+    #save product id in shop metafield for liquid template to consume
+    shop = session[:shopify].shop
+
+    if (@vars.length > 0)
+      fields = shop.metafields
+      field = fields.find { |f| f.key == 'product_id' && f.namespace ='AusPostShipping'}
+      
+      if field.nil?      
+        field = ShopifyAPI::Metafield.new({:namespace =>'AusPostShipping',:key=>'product_id', :value=>@vars[0].id, :value_type=>'string' })
+        field.save
+      elsif (field.value.to_s != @vars[0].id.to_s) #only save if variant id has changed
+        field.destroy
+        field = ShopifyAPI::Metafield.new({:namespace =>'AusPostShipping',:key=>'product_id', :value=>@vars[0].id, :value_type=>'string' })
+        field.save!
+      end
+    end
+        
+  end
+  
   def update_shop_metafield(default_charge)
     shop = session[:shopify].shop
     fields = shop.metafields
