30617eab6a2cbbfb12a47bd46d075ddf32f8c047
Richard Zhou <richard.zhou@sage.com>
2013-03-11 17:42:51 -0700

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ WorldShippingCalculator::Application.routes.draw do
   match "help" => 'help#index'
   match "confirm_charge" => "home#confirm_charge"
   
-  match "/shipping_rates" => "rates#shipping_rates", :via => :post #Consider removing the external
+  match "/rates" => "rates#shipping_rates", :via => :post #Consider removing the external
   
 
   resource 'preferences', only: [:show, :edit, :update]
