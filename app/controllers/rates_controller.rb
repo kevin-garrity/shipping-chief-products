ecc15bdd7d2fa467792f470e9d6889da85ec9ba2
Zeigfreid <various.authors@gmail.com>
2013-03-14 13:59:06 -0700

    More tests, and some changes to rates_controller
    I added a check for the rate param being nil, as well as a rescue block
    at the end to receive some of Fedex's errors.

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -4,44 +4,62 @@ include ActiveMerchant::Shipping
 class RatesController < ApplicationController
 
   def shipping_rates
-    puts("-------request is" + params.to_s)
 
-    in_origin = params[:rate][:origin]
-    in_dest = params[:rate][:destination]
+    rate = params[:rate]
+    puts "======================="
+    puts "rate: " + rate.inspect
 
-    origin = Location.new( in_origin)
-    destination = Location.new( in_dest)
+    if not rate.nil?
 
-    rate_array = Array.new
-    rate_array <<
-    {
-      'service_name' => 'canadapost-overnight',
-      'service_code' => 'ON',
-      'total_price' =>  '12.95',
-      'currency' => 'USD'
-    }
+      in_origin = rate[:origin]
+      in_dest = rate[:destination]
 
-    rate_array <<
-    {
-      'service_name' => 'canadapost-2dayground',
-      'service_code' => '1D',
-      'total_price' => '29.34',
-      'currency' => 'USD'
-    }
-    packages = [
-      Package.new(  100,                        # 100 grams
-                  [93,10],                    # 93 cm long, 10 cm diameter
-                  :cylinder => true),         # cylinders have different volume calculations
+      puts "======================="
+      puts "origin: " + in_origin.inspect
+      puts "dest: " + in_dest.inspect
 
-                  Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-                              [15, 10, 4.5],              # 15x10x4.5 inches
-                              :units => :imperial)        # not grams, not centimetres
-    ]
-    fedex = FedexRate.new()
-    rates = fedex.get_rates(origin, destination, packages)
-    puts("--rate from fedex is " + rates.to_s)
+      origin = Location.new( in_origin)
+      destination = Location.new( in_dest)
 
-    #puts("----- returning " + rates.to_json)
-    render :json => {:rates => rates}
+      rate_array = Array.new
+      rate_array <<
+      {
+        'service_name' => 'canadapost-overnight',
+        'service_code' => 'ON',
+        'total_price' =>  '12.95',
+        'currency' => 'USD'
+      }
+
+      rate_array <<
+      {
+        'service_name' => 'canadapost-2dayground',
+        'service_code' => '1D',
+        'total_price' => '29.34',
+        'currency' => 'USD'
+      }
+      packages = [
+        Package.new(  100,                        # 100 grams
+                      [93,10],                    # 93 cm long, 10 cm diameter
+                      :cylinder => true),         # cylinders have different volume calculations
+
+        Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
+                      [15, 10, 4.5],              # 15x10x4.5 inches
+                      :units => :imperial)        # not grams, not centimetres
+      ]
+      fedex = FedexRate.new()
+      rates = fedex.get_rates(origin, destination, packages)
+      puts("--rate from fedex is " + rates.to_s)
+
+      #puts("----- returning " + rates.to_json)
+      render :json => {:rates => rates}
+    else
+
+      render nothing: true
+    end
+
+  rescue ActiveMerchant::Shipping::ResponseError => e
+    puts e.message
+    render nothing: true
+
   end
 end
--- spec/controllers/rates_controller_spec.rb
+++ spec/controllers/rates_controller_spec.rb
@@ -69,7 +69,26 @@ describe RatesController do
       it "should render NOTHING" do
         post :shipping_rates
 
-        response.should render_nothing
+        response.body.strip.should be_empty
+      end
+    end
+
+    context "when shopify passes us incomplete params" do
+
+      it "should render NOTHING when origin is missing" do
+        invalid_params = valid_parameters_from_shopify
+        invalid_params["rate"]["origin"] = {}
+        post :shipping_rates, invalid_params
+
+        response.body.strip.should be_empty
+      end
+
+      it "should render NOTHING when destination is missing" do
+        invalid_params = valid_parameters_from_shopify
+        invalid_params["rate"]["destination"] = {}
+        post :shipping_rates, invalid_params
+
+        response.body.strip.should be_empty
       end
     end
   end
