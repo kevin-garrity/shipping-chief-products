6f9520545fe5b626815e800bdc64fb11020e5bde
Richard Zhou <richard.zhou@sage.com>
2013-03-13 22:03:29 -0700

    output sample rates

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,7 +1,7 @@
 class RatesController < ApplicationController
 
   def shipping_rates
-    logger.debug("-------request is" + params.to_s)
+    puts("-------request is" + params.to_s)
     #fedex = FedexRate.new()
     #fedex.get_rates
 
