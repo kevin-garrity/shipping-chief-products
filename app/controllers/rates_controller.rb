d2bd8b5d36748017bfd69b4bda737776c1ac0f6a
Richard Zhou <richard.zhou@sage.com>
2013-03-15 12:30:18 -0700

    more debug code

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -34,7 +34,9 @@ class RatesController < ApplicationController
         # look up one package and multiple by quantity
         quan = item[:quantity].to_i
         puts("quan is " + quan.to_s)
-        packages << Package.new(item[:grams].to_i/quan, [])
+        single_item_weight = item[:grams].to_i/quan
+        puts("single_item_weight is " + single_item_weight.to_s)
+        packages << Package.new(single_item_weight, [])
         single_rate = fedex.get_rates(origin, destination, packages) 
         rates = single_rate.collect do | rate|
           {"service_name" => rate["service_name"], 'service_code'=> rate["service_code"], 'total_price' => rate["price"].to_i * quan, 'currency' => rate["currency"]}
