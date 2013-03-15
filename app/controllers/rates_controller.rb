f7d3cc01ac1f46db37c71c2633e5e3999ea145dd
Richard Zhou <richard.zhou@sage.com>
2013-03-15 15:32:37 -0700

    more debug information

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -11,7 +11,7 @@ class RatesController < ApplicationController
     end
     
     puts("shipping origin is" + rate[:origin].to_s)
-    puts("shipping destination is" + rate[:origin].to_s)
+    puts("shipping destination is" + rate[:destination].to_s)
     puts("shipping items is" + rate[:items].to_s)
 
     in_origin = rate[:origin]
@@ -28,17 +28,17 @@ class RatesController < ApplicationController
       # treat each item as seperate 
       packages = Array.new      
       fedex = FedexRate.new()
+      quan = item[:quantity].to_i
+      
       # get the number of items being ordered
       if (item[:sku].start_with?("SAM/")) #shipped together
-        packages << Package.new(item[:grams].to_i, [])
+        packages << Package.new(item[:grams].to_i * quan, [])
         rates = fedex.get_rates(origin, destination, packages)
       else
         # look up one package and multiple by quantity
-        quan = item[:quantity].to_i
         puts("quan is " + quan.to_s)
-        single_item_weight = item[:grams].to_i/quan
-        puts("single_item_weight is " + single_item_weight.to_s)
-        packages << Package.new(single_item_weight, [])
+
+        packages << Package.new(item[:grams].to_i, [])
         single_rate = fedex.get_rates(origin, destination, packages) 
         rates = single_rate.collect do | rate|
           {"service_name" => rate["service_name"], 'service_code'=> rate["service_code"], 'total_price' => rate["total_price"].to_i * quan, 'currency' => rate["currency"]}
