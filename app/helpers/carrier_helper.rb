4045b3dca469eb69df1ef53389cd4cd35eb1c701
Richard Zhou <richard.zhou@sage.com>
2013-04-28 22:28:45 -0700

    conver to symbol
    Conflicts:
    
    	app/helpers/carrier_helper.rb

--- app/helpers/carrier_helper.rb
+++ app/helpers/carrier_helper.rb
@@ -2,7 +2,7 @@ module CarrierHelper
   def carrier_name_for(name, config=nil)
     config ||= client_config
     name = config.carriers.detect { |key|
-      name.to_s == key.to_s || ( AppConfig.carriers[key].legacy_name && (AppConfig.carriers[key].legacy_name == name.to_s ))
+      name.to_s == key.to_s || ( AppConfig.carriers[key.to_s].legacy_name && (AppConfig.carriers[key.to_s].legacy_name == name.to_s ))
     }.to_s
   end
 
