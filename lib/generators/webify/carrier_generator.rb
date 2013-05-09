dcdfa5741914ffa54b4a10b9fc91e542950ced40
Michael Johnston <lastobelus@mac.com>
2013-05-09 01:30:21 -0700

    add USAGE for generator

--- lib/generators/webify/carrier_generator.rb
+++ lib/generators/webify/carrier_generator.rb
@@ -6,8 +6,8 @@ class Webify::CarrierGenerator < Rails::Generators::NamedBase
   class_option :service_host, type: :string, default: 'webify-shipping.herokuapp.com', description: "Host for the rates service for production"  
  class_option :staging_service_host, type: :string, default: 'shipping-staging.herokuapp.com', description: "Host for the rates service for staging"  
 
-  class_option :shop, type: :string, description: "Shop to grant access for production" 
-  class_option :staging_shop, type: :string, description: "Shop to grant access for staging", required: false
+  class_option :shop, type: :string, description: "Shop to grant access for production. Will be added to app_config_production -- set to 'none' to skip." 
+  class_option :staging_shop, type: :string, description: "Shop to grant access for staging. Will be added to app_config_staging -- set to 'none' to skip.", required: false
 
   def create_files
     carrier_dir = "app/models/carriers/#{name.underscore}"
@@ -36,37 +36,41 @@ class Webify::CarrierGenerator < Rails::Generators::NamedBase
       say_status(:app_config, "add carrier #{name.underscore}", :green)
       File.open(app_config_path, 'w'){|f| f.write app_config.to_yaml}
     end
-    prod_config_path = 'config/app_config_production.yaml'
-    prod_config = YAML::load_file(prod_config_path)
-    prod_config['clients'] ||= {}
-    if prod_config['clients'][shop]
-      say_status(:app_config, "production: client #{shop} already exists", :blue)
-    else
-      prod_config['clients'][shop] = {
-        'access' => {'all_except' => []},
-        'menus' => [],
-        'carriers' => [name.underscore.to_sym],
-        'service_host' => options.service_host
-      }
-      say_status(:app_config, "production: add client #{shop}", :green)
-      File.open(prod_config_path, 'w'){|f| f.write prod_config.to_yaml}
+
+    unless options.shop == "none"
+      prod_config_path = 'config/app_config_production.yaml'
+      prod_config = YAML::load_file(prod_config_path)
+      prod_config['clients'] ||= {}
+      if prod_config['clients'][shop]
+        say_status(:app_config, "production: client #{shop} already exists", :blue)
+      else
+        prod_config['clients'][shop] = {
+          'access' => {'all_except' => []},
+          'menus' => [],
+          'carriers' => [name.underscore.to_sym],
+          'service_host' => options.service_host
+        }
+        say_status(:app_config, "production: add client #{shop}", :green)
+        File.open(prod_config_path, 'w'){|f| f.write prod_config.to_yaml}
+      end
     end
 
-    staging_config_path = 'config/app_config_staging.yaml'
-    staging_config = YAML::load_file(staging_config_path)
-    staging_config['clients'] ||= {}
-    if staging_config['clients'][staging_shop]
-      say_status(:app_config, "staging: client #{staging_shop} already exists", :blue)
-    else
-      staging_config['clients'][staging_shop] = {
-        'access' => {'all_except' => []},
-        'menus' => [],
-        'carriers' => [name.underscore.to_sym],
-        'service_host' => options.staging_service_host
-      }
-      say_status(:app_config, "staging: add client #{staging_shop}", :green)
-      File.open(staging_config_path, 'w'){|f| f.write staging_config.to_yaml}
+    unless options.staging == "none"
+      staging_config_path = 'config/app_config_staging.yaml'
+      staging_config = YAML::load_file(staging_config_path)
+      staging_config['clients'] ||= {}
+      if staging_config['clients'][staging_shop]
+        say_status(:app_config, "staging: client #{staging_shop} already exists", :blue)
+      else
+        staging_config['clients'][staging_shop] = {
+          'access' => {'all_except' => []},
+          'menus' => [],
+          'carriers' => [name.underscore.to_sym],
+          'service_host' => options.staging_service_host
+        }
+        say_status(:app_config, "staging: add client #{staging_shop}", :green)
+        File.open(staging_config_path, 'w'){|f| f.write staging_config.to_yaml}
+      end
     end
-
   end
 end
