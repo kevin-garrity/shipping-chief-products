79bc33404787058ffdcd80f0adf1b4cbbef4adf2
Michael Johnston <lastobelus@mac.com>
2013-05-10 13:54:26 -0700

    WIP: rufus

--- app/models/carriers/rufus_service.rb
+++ app/models/carriers/rufus_service.rb
@@ -1,7 +1,8 @@
 module Carriers
   class RufusService < ::Carriers::Service
     def fetch_rates
-      # 
+      construct_aggregate_columns if preference.aggregate_columns?
+
       # for each item
       #    optionally: restore option1, option2, option3 & product_type
       #    map item keys
@@ -12,6 +13,11 @@ module Carriers
 
     def decision_table_dir
       Rails.root.join( 'rufus', self.class.name.demodulize.underscore)
+    end
+
+    def construct_aggregate_columns
+      # for lifemap we need "Product Types" which is a set of the product types in the order
+      #  Shopify
     end
 
     def decisions
