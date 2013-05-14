2ca792043a074e66abf89ca04ba19abdd13d2bf7
Michael Johnston <lastobelus@mac.com>
2013-05-13 18:13:23 -0700

    product_cache by variant by item.name

--- app/models/product_cache.rb
+++ app/models/product_cache.rb
@@ -2,10 +2,11 @@
   shopify does not send enough information, so we have to maintain a product cache
 =end
 require 'logger'
+require 'dalli'
 
 class ProductCache
 
-  attr_accessor :domain, :opts
+  attr_accessor :domain, :opts, :logger
 
   def initialize(opts={})
     @domain = ShopifyAPI::Shop.current.myshopify_domain
@@ -21,8 +22,31 @@ class ProductCache
     "#{domain}-products"
   end
 
+  def variants_key
+    "#{domain}-variants"
+  end
+
+
   def product_fields
     opts[:product_fields] || "id,product_type,title,options,variants,vendor"
+  end
+
+  def variants
+    @variants ||= begin
+      products = ShopifyAPI::Product.find(:all,
+        params: {limit: false, fields: product_fields}
+      )
+      puts "products.map(&:id): #{products.map(&:id).inspect}"
+      @variants = {}
+      products.each do |product|
+        product.variants.each do |variant|
+          variant.attributes[:product] = product
+          item_title = [product.title, variant.title].join(' - ')
+          @variants[item_title] = variant
+        end
+      end
+      @variants
+    end
   end
 
   def products
@@ -34,16 +58,16 @@ class ProductCache
         params: {limit: false, fields: product_fields}
       )
       cache.set(products_key, @products)
+      @products
     end
-    @products
   end
 
   def[](shopify_rates_params_item)
-    
+    self.variants[shopify_rates_params_item['name']]
   end
 
   def dirty!
-    @products = nil
+    @variants = nil
   end
 
 end
